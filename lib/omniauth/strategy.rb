75e09cecac35481317b53e3c87f4879ec40247be
Matt E. Patterson <madraziel@gmail.com>
2012-10-04 11:39:30 -0500

    adds rescue for bad uri's so we can deal with them higher up, hopefully

--- lib/omniauth/strategy.rb
+++ lib/omniauth/strategy.rb
@@ -395,18 +395,22 @@ module OmniAuth
     end
 
     def full_host
-      case OmniAuth.config.full_host
-        when String
-          OmniAuth.config.full_host
-        when Proc
-          OmniAuth.config.full_host.call(env)
-        else
-          uri = URI.parse(request.url.gsub(/\?.*$/,''))
-          uri.path = ''
-          uri.query = nil
-          #sometimes the url is actually showing http inside rails because the other layers (like nginx) have handled the ssl termination.
-          uri.scheme = 'https' if(request.env['HTTP_X_FORWARDED_PROTO'] == 'https')          
-          uri.to_s
+      begin
+        case OmniAuth.config.full_host
+          when String
+            OmniAuth.config.full_host
+          when Proc
+            OmniAuth.config.full_host.call(env)
+          else
+            uri = URI.parse(request.url.gsub(/\?.*$/,''))
+            uri.path = ''
+            uri.query = nil
+            #sometimes the url is actually showing http inside rails because the other layers (like nginx) have handled the ssl termination.
+            uri.scheme = 'https' if(request.env['HTTP_X_FORWARDED_PROTO'] == 'https')          
+            uri.to_s
+        end
+      rescue URI::InvalidURIError
+        raise ProtocolError, "request.url is not a valid URI: #{request.url}"
       end
     end
 
